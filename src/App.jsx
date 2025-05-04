5c3f3d41e3ac47045a808d8c47f6b9e338f1c592
Paul Ember <133266739+paulember@users.noreply.github.com>
2025-05-04 21:31:59 +0000

    added vennMiss & vennMatch to style css new useFetchWine component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import './App.css';
 import './style.css';
+
 import { useState, useEffect, useSyncExternalStore } from "react";
 
+import useFetchWine from "./component/useFetchWine";
+
 export default function App() {
+  const { wineData, loading, error } = useFetchWine();
+  const [venntdClass, setVenntdClass] = useState([Array(6).fill(null)]);
+
+  
+  useEffect(() => {
+    const tempArray = [];
+    for (let i = 0; i < 6; i++) {
+      tempArray[i] = "td-vennMiss";
+    }
+
+    setVenntdClass([
+      tempArray[0],
+      tempArray[1],
+      tempArray[2],
+      tempArray[3],
+      tempArray[4],
+      tempArray[5]
+    ]);
+  }, []);
+
   return (
     <div className="App">
       <div>
         <div>
           <table class="notesTable">
             <tr>
-            <td class="td-wineMiss"> lab1 </td>
-            <td class="td-wineMiss"> lab2 </td>
-            <td class="td-wineMiss"> lab3 </td>
+            <td class={venntdClass[0]}> lab1 </td>
+            <td class={venntdClass[1]}> lab2 </td>
+            <td class={venntdClass[2]}> lab3 </td>
             </tr>
             <tr>
-            <td class="td-wineMiss"> lab4 </td>
-            <td class="td-wineMiss"> lab5 </td>
-            <td class="td-wineMiss"> lab6 </td>
+            <td class={venntdClass[3]}> lab4 </td>
+            <td class={venntdClass[4]}> lab5 </td>
+            <td class={venntdClass[5]}>  lab6 </td>
             </tr>
           </table>
         </div>
