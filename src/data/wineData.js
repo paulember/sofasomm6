bf970ebbe5e216abbd6477b913e01934716ca4a6
Paul Ember <133266739+paulember@users.noreply.github.com>
2025-05-05 14:50:13 -0400

    Create wineData.js

---
+++ src/data/wineData.js
@@ -0,0 +1,792 @@
+export const wineData = [
+  {
+    id: "1",
+    style: "Airen",
+    tastingNote1: "Apple",
+    tastingNote2: "Pineapple",
+    tastingNote3: "Grapefruit",
+    tastingNote4: "Banana",
+    tastingNote5: "Rose",
+    "returnColumn\r": "Airen\r"
+  },
+  {
+    id: "2",
+    style: "Albariño",
+    tastingNote1: "Lemon Zest",
+    tastingNote2: "Grapefruit",
+    tastingNote3: "Honeydew",
+    tastingNote4: "Nectarine",
+    tastingNote5: "Saline",
+    "returnColumn\r": "Albariño\r"
+  },
+  {
+    id: "3",
+    style: "Barbera",
+    tastingNote1: "Tart Cherry",
+    tastingNote2: "Licorice",
+    tastingNote3: "Blackberry",
+    tastingNote4: "Dried Herbs",
+    tastingNote5: "Black Pepper",
+    "returnColumn\r": "Barbera\r"
+  },
+  {
+    id: "4",
+    style: "Beaujolais",
+    tastingNote1: "Pomegranate",
+    tastingNote2: "Blackberry Bramble",
+    tastingNote3: "Violet",
+    tastingNote4: "Potting Soil",
+    tastingNote5: "Banana",
+    "returnColumn\r": "Beaujolais\r"
+  },
+  {
+    id: "5",
+    style: "Bordeaux Blend",
+    tastingNote1: "Black Currant",
+    tastingNote2: "Black Cherry",
+    tastingNote3: "Graphite",
+    tastingNote4: "Chocolate",
+    tastingNote5: "Dried Herbs",
+    "returnColumn\r": "Bordeaux Blend\r"
+  },
+  {
+    id: "6",
+    style: "Cabernet Franc",
+    tastingNote1: "Strawberry",
+    tastingNote2: "Raspberry",
+    tastingNote3: "Bell Pepper",
+    tastingNote4: "Crushed Gravel",
+    tastingNote5: "Chili Pepper",
+    "returnColumn\r": "Cabernet Franc\r"
+  },
+  {
+    id: "7",
+    style: "Cabernet Sauvignon",
+    tastingNote1: "Black Cherry",
+    tastingNote2: "Black Currant",
+    tastingNote3: "Cedar",
+    tastingNote4: "Baking Spices",
+    tastingNote5: "Graphite",
+    "returnColumn\r": "Cabernet Sauvignon\r"
+  },
+  {
+    id: "8",
+    style: "Carignan",
+    tastingNote1: "Dried Cranberry",
+    tastingNote2: "Raspberry",
+    tastingNote3: "Tobacco Leaf",
+    tastingNote4: "Baking Spices",
+    tastingNote5: "Cured Meat",
+    "returnColumn\r": "Carignan\r"
+  },
+  {
+    id: "9",
+    style: "Carménère",
+    tastingNote1: "Raspberry",
+    tastingNote2: "Bell Pepper",
+    tastingNote3: "Black Plum",
+    tastingNote4: "Vanilla",
+    tastingNote5: "Paprika",
+    "returnColumn\r": "Carménère\r"
+  },
+  {
+    id: "10",
+    style: "Cava",
+    tastingNote1: "Quince",
+    tastingNote2: "Lime",
+    tastingNote3: "Yellow Apple",
+    tastingNote4: "Chamomile",
+    tastingNote5: "Almond",
+    "returnColumn\r": "Cava\r"
+  },
+  {
+    id: "11",
+    style: "Chablis",
+    tastingNote1: "Quince",
+    tastingNote2: "Starfruit",
+    tastingNote3: "Lime Peel",
+    tastingNote4: "White Flowers",
+    tastingNote5: "Chalk",
+    "returnColumn\r": "Chablis\r"
+  },
+  {
+    id: "12",
+    style: "Chambourcin",
+    tastingNote1: "Black Cherry",
+    tastingNote2: "Black Pepper",
+    tastingNote3: "Crushed Gravel",
+    tastingNote4: "Wet Loam",
+    tastingNote5: "Chocolate",
+    "returnColumn\r": "Chambourcin\r"
+  },
+  {
+    id: "13",
+    style: "Champagne",
+    tastingNote1: "Citrus",
+    tastingNote2: "Yellow Apple",
+    tastingNote3: "Cream",
+    tastingNote4: "Almond",
+    tastingNote5: "Toast",
+    "returnColumn\r": "Champagne\r"
+  },
+  {
+    id: "14",
+    style: "Chardonnay",
+    tastingNote1: "Lemon",
+    tastingNote2: "Yellow Apple",
+    tastingNote3: "Butter",
+    tastingNote4: "Vanilla",
+    tastingNote5: "Pineapple",
+    "returnColumn\r": "Chardonnay\r"
+  },
+  {
+    id: "15",
+    style: "Chenin Blanc",
+    tastingNote1: "Quince",
+    tastingNote2: "Yellow Apple",
+    tastingNote3: "Pear",
+    tastingNote4: "Chamomile",
+    tastingNote5: "Honey",
+    "returnColumn\r": "Chenin Blanc\r"
+  },
+  {
+    id: "16",
+    style: "Cinsault",
+    tastingNote1: "Raspberry",
+    tastingNote2: "Red Currant",
+    tastingNote3: "Tart Cherry",
+    tastingNote4: "Violet",
+    tastingNote5: "Black Tea",
+    "returnColumn\r": "Cinsault\r"
+  },
+  {
+    id: "17",
+    style: "Concord",
+    tastingNote1: "Grape",
+    tastingNote2: "Plum Sauce",
+    tastingNote3: "Musk",
+    tastingNote4: "Potting Soil",
+    tastingNote5: "Cumin",
+    "returnColumn\r": "Concord\r"
+  },
+  {
+    id: "18",
+    style: "Côtes du Rhône Blend",
+    tastingNote1: "Raspberry",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Rosemary",
+    tastingNote4: "Baking Spices",
+    tastingNote5: "Lavender",
+    "returnColumn\r": "Côtes du Rhône Blend\r"
+  },
+  {
+    id: "19",
+    style: "Crémant",
+    tastingNote1: "Lemon",
+    tastingNote2: "White Peach",
+    tastingNote3: "White Cherry",
+    tastingNote4: "Almond",
+    tastingNote5: "Toast",
+    "returnColumn\r": "Crémant\r"
+  },
+  {
+    id: "20",
+    style: "Franciacorta",
+    tastingNote1: "Lemon",
+    tastingNote2: "Peach",
+    tastingNote3: "White Cherry",
+    tastingNote4: "Almond",
+    tastingNote5: "Toast",
+    "returnColumn\r": "Franciacorta\r"
+  },
+  {
+    id: "21",
+    style: "Furmint",
+    tastingNote1: "Meyer Lemon",
+    tastingNote2: "Green Apple",
+    tastingNote3: "Ginger",
+    tastingNote4: "Smoke",
+    tastingNote5: "Pepperocini",
+    "returnColumn\r": "Furmint\r"
+  },
+  {
+    id: "22",
+    style: "Gamay",
+    tastingNote1: "Pomegranate",
+    tastingNote2: "Blackberry Bramble",
+    tastingNote3: "Violet",
+    tastingNote4: "Potting Soil",
+    tastingNote5: "Peony",
+    "returnColumn\r": "Gamay\r"
+  },
+  {
+    id: "23",
+    style: "Gargenega",
+    tastingNote1: "Peach",
+    tastingNote2: "Honeydew",
+    tastingNote3: "Tangerine",
+    tastingNote4: "Marjoram",
+    tastingNote5: "Saline",
+    "returnColumn\r": "Gargenega\r"
+  },
+  {
+    id: "24",
+    style: "Gewürztraminer",
+    tastingNote1: "Lychee",
+    tastingNote2: "Rose",
+    tastingNote3: "Grapefruit",
+    tastingNote4: "Tangerine",
+    tastingNote5: "Ginger",
+    "returnColumn\r": "Gewürztraminer\r"
+  },
+  {
+    id: "25",
+    style: "Grenache",
+    tastingNote1: "Stewed Strawberry",
+    tastingNote2: "Grilled Plum",
+    tastingNote3: "Leather",
+    tastingNote4: "Dried Herbs",
+    tastingNote5: "Blood Orange",
+    "returnColumn\r": "Grenache\r"
+  },
+  {
+    id: "26",
+    style: "Grillo",
+    tastingNote1: "Grapefruit",
+    tastingNote2: "Citrus Blossom",
+    tastingNote3: "Thyme",
+    tastingNote4: "Minerals",
+    tastingNote5: "Saline",
+    "returnColumn\r": "Grillo\r"
+  },
+  {
+    id: "27",
+    style: "Grüner Veltliner",
+    tastingNote1: "Starfruit",
+    tastingNote2: "Gooseberry",
+    tastingNote3: "Green Bean",
+    tastingNote4: "White Pepper",
+    tastingNote5: "Crushed Gravel",
+    "returnColumn\r": "Grüner Veltliner\r"
+  },
+  {
+    id: "28",
+    style: "Lambrusco",
+    tastingNote1: "Strawberry",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Rhubarb",
+    tastingNote4: "Hibiscus",
+    tastingNote5: "Potting Soil",
+    "returnColumn\r": "Lambrusco\r"
+  },
+  {
+    id: "29",
+    style: "Madeira",
+    tastingNote1: "Burnt Caramel",
+    tastingNote2: "Walnut Oil",
+    tastingNote3: "Peach",
+    tastingNote4: "Hazelnut",
+    tastingNote5: "Orange Peel",
+    "returnColumn\r": "Madeira\r"
+  },
+  {
+    id: "30",
+    style: "Malbec",
+    tastingNote1: "Red Plum",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Vanilla",
+    tastingNote4: "Sweet Tobacco",
+    tastingNote5: "Cocoa",
+    "returnColumn\r": "Malbec\r"
+  },
+  {
+    id: "31",
+    style: "Marsala",
+    tastingNote1: "Stewed Apricot",
+    tastingNote2: "Vanilla",
+    tastingNote3: "Tamarind",
+    tastingNote4: "Brown Sugar",
+    tastingNote5: "Tobacco",
+    "returnColumn\r": "Marsala\r"
+  },
+  {
+    id: "32",
+    style: "Marsanne",
+    tastingNote1: "Quince",
+    tastingNote2: "Mandarin Orange",
+    tastingNote3: "Apricot",
+    tastingNote4: "Acacia",
+    tastingNote5: "Beeswax",
+    "returnColumn\r": "Marsanne\r"
+  },
+  {
+    id: "33",
+    style: "Melon de Bourgogne",
+    tastingNote1: "Lime",
+    tastingNote2: "Seashell",
+    tastingNote3: "Green Apple",
+    tastingNote4: "Pear",
+    tastingNote5: "Bread Dough",
+    "returnColumn\r": "Melon de Bourgogne\r"
+  },
+  {
+    id: "34",
+    style: "Meritage",
+    tastingNote1: "Black Currant",
+    tastingNote2: "Black Cherry",
+    tastingNote3: "Graphite",
+    tastingNote4: "Chocolate",
+    tastingNote5: "Dried Herbs",
+    "returnColumn\r": "Meritage\r"
+  },
+  {
+    id: "35",
+    style: "Merlot",
+    tastingNote1: "Cherry",
+    tastingNote2: "Chocolate",
+    tastingNote3: "Plum",
+    tastingNote4: "Bay Leaf",
+    tastingNote5: "Vanilla",
+    "returnColumn\r": "Merlot\r"
+  },
+  {
+    id: "36",
+    style: "Montepulciano",
+    tastingNote1: "Red Plum",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Dried Thyme",
+    tastingNote4: "Baking Spices",
+    tastingNote5: "Mesquite",
+    "returnColumn\r": "Montepulciano\r"
+  },
+  {
+    id: "37",
+    style: "Mourvèdre",
+    tastingNote1: "Blackberry",
+    tastingNote2: "Black Pepper",
+    tastingNote3: "Cocoa",
+    tastingNote4: "Tobacco",
+    tastingNote5: "Roasted Meat",
+    "returnColumn\r": "Mourvèdre\r"
+  },
+  {
+    id: "38",
+    style: "Muscadet",
+    tastingNote1: "Lime",
+    tastingNote2: "Seashell",
+    tastingNote3: "Green Apple",
+    tastingNote4: "Pear",
+    tastingNote5: "Bread Dough",
+    "returnColumn\r": "Muscadet\r"
+  },
+  {
+    id: "39",
+    style: "Muscat Blanc",
+    tastingNote1: "Orange Blossum",
+    tastingNote2: "Meyer Lemon",
+    tastingNote3: "Mandarin Orange",
+    tastingNote4: "Pear",
+    tastingNote5: "Honeysuckle",
+    "returnColumn\r": "Muscat Blanc\r"
+  },
+  {
+    id: "40",
+    style: "Muscat of Alexandria",
+    tastingNote1: "Mandarin Orange",
+    tastingNote2: "Honey",
+    tastingNote3: "Lychee",
+    tastingNote4: "Peach Skin",
+    tastingNote5: "White Flowers",
+    "returnColumn\r": "Muscat of Alexandria\r"
+  },
+  {
+    id: "41",
+    style: "Nebbiolo",
+    tastingNote1: "Cherry",
+    tastingNote2: "Rose",
+    tastingNote3: "Leather",
+    tastingNote4: "Anise",
+    tastingNote5: "Clay Pot",
+    "returnColumn\r": "Nebbiolo\r"
+  },
+  {
+    id: "42",
+    style: "Nero d’Avola",
+    tastingNote1: "Black Cherry",
+    tastingNote2: "Black Plum",
+    tastingNote3: "Licorice",
+    tastingNote4: "Tobacco",
+    tastingNote5: "Chili Pepper",
+    "returnColumn\r": "Nero d’Avola\r"
+  },
+  {
+    id: "42.5",
+    style: "Norton",
+    tastingNote1: "Black Currant",
+    tastingNote2: "Black Cherry",
+    tastingNote3: "Dill",
+    tastingNote4: "Black Pepper",
+    tastingNote5: "Chocolate",
+    "returnColumn\r": "ZZ\r"
+  },
+  {
+    id: "43",
+    style: "Pedro Ximénez",
+    tastingNote1: "Toffee",
+    tastingNote2: "Figs",
+    tastingNote3: "Walnut",
+    tastingNote4: "Lemon",
+    tastingNote5: "Coffee",
+    "returnColumn\r": "Pedro Ximénez\r"
+  },
+  {
+    id: "44",
+    style: "Petit Verdot",
+    tastingNote1: "Black Cherry",
+    tastingNote2: "Plum",
+    tastingNote3: "Violet",
+    tastingNote4: "Lilac",
+    tastingNote5: "Sage",
+    "returnColumn\r": "Petit Verdot\r"
+  },
+  {
+    id: "45",
+    style: "Petite Sirah",
+    tastingNote1: "Sugarplum",
+    tastingNote2: "Blueberry",
+    tastingNote3: "Dark Chocolate",
+    tastingNote4: "Black Pepper",
+    tastingNote5: "Black Tea",
+    "returnColumn\r": "Petite Sirah\r"
+  },
+  {
+    id: "46",
+    style: "Picpoul",
+    tastingNote1: "Green Apple",
+    tastingNote2: "Citrus Blossom",
+    tastingNote3: "Lemon",
+    tastingNote4: "Thyme",
+    tastingNote5: "Saline",
+    "returnColumn\r": "Picpoul\r"
+  },
+  {
+    id: "47",
+    style: "Pinot Blanc",
+    tastingNote1: "Pear",
+    tastingNote2: "Peach",
+    tastingNote3: "Raw Almond",
+    tastingNote4: "Lemon Zest",
+    tastingNote5: "Crushed Gravel",
+    "returnColumn\r": "Pinot Blanc\r"
+  },
+  {
+    id: "48",
+    style: "Pinot Gris / Pinot Grigio",
+    tastingNote1: "White Peach",
+    tastingNote2: "Lemon Zest",
+    tastingNote3: "Cantaloupe",
+    tastingNote4: "Green Almond",
+    tastingNote5: "Crushed Gravel",
+    "returnColumn\r": "Pinot Gris / Pinot Grigio\r"
+  },
+  {
+    id: "49",
+    style: "Pinot Noir",
+    tastingNote1: "Raspberry",
+    tastingNote2: "Cherry",
+    tastingNote3: "Mushroom",
+    tastingNote4: "Vanilla",
+    tastingNote5: "Hibiscus",
+    "returnColumn\r": "Pinot Noir\r"
+  },
+  {
+    id: "50",
+    style: "Pinotage",
+    tastingNote1: "Black Cherry",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Fig",
+    tastingNote4: "Mint",
+    tastingNote5: "Game",
+    "returnColumn\r": "Pinotage\r"
+  },
+  {
+    id: "51",
+    style: "Port",
+    tastingNote1: "Plum",
+    tastingNote2: "Dried Cherry",
+    tastingNote3: "Chocolate",
+    tastingNote4: "Raisin",
+    tastingNote5: "Cinnamon",
+    "returnColumn\r": "Port\r"
+  },
+  {
+    id: "52",
+    style: "Prosecco",
+    tastingNote1: "Green Apple",
+    tastingNote2: "Honeydew",
+    tastingNote3: "Pear",
+    tastingNote4: "Lager",
+    tastingNote5: "Cream",
+    "returnColumn\r": "Prosecco\r"
+  },
+  {
+    id: "53",
+    style: "Provençal Rosé",
+    tastingNote1: "Strawberry",
+    tastingNote2: "Honeydew Melon",
+    tastingNote3: "Rose Petal",
+    tastingNote4: "Celery",
+    tastingNote5: "Watermelon",
+    "returnColumn\r": "Provençal Rosé\r"
+  },
+  {
+    id: "54",
+    style: "Riesling",
+    tastingNote1: "Lime",
+    tastingNote2: "Green Apple",
+    tastingNote3: "Beeswax",
+    tastingNote4: "Jasmine",
+    tastingNote5: "Petroleum",
+    "returnColumn\r": "Riesling\r"
+  },
+  {
+    id: "55",
+    style: "Rioja (Red)",
+    tastingNote1: "Cherry",
+    tastingNote2: "Plum",
+    tastingNote3: "Dill",
+    tastingNote4: "Vanilla",
+    tastingNote5: "Leather",
+    "returnColumn\r": "Rioja (Red)\r"
+  },
+  {
+    id: "56",
+    style: "Roussanne",
+    tastingNote1: "Meyer Lemon",
+    tastingNote2: "Apricot",
+    tastingNote3: "Beeswax",
+    tastingNote4: "Chamomile",
+    tastingNote5: "Brioche",
+    "returnColumn\r": "Roussanne\r"
+  },
+  {
+    id: "57",
+    style: "Sancerre",
+    tastingNote1: "Gooseberry",
+    tastingNote2: "Grapefruit",
+    tastingNote3: "Honeydew",
+    tastingNote4: "Hay",
+    tastingNote5: "Flint",
+    "returnColumn\r": "Sancerre\r"
+  },
+  {
+    id: "58",
+    style: "Sangiovese",
+    tastingNote1: "Cherry",
+    tastingNote2: "Roasted Tomato",
+    tastingNote3: "Sweet Balsamic",
+    tastingNote4: "Oregano",
+    tastingNote5: "Espresso",
+    "returnColumn\r": "Sangiovese\r"
+  },
+  {
+    id: "59",
+    style: "Sauternais",
+    tastingNote1: "Lemon Curd",
+    tastingNote2: "Apricot",
+    tastingNote3: "Quince",
+    tastingNote4: "Honey",
+    tastingNote5: "Ginger",
+    "returnColumn\r": "Sauternais\r"
+  },
+  {
+    id: "60",
+    style: "Sauvignon Blanc",
+    tastingNote1: "Gooseberry",
+    tastingNote2: "Honeydew",
+    tastingNote3: "Grapefruit",
+    tastingNote4: "White Peach",
+    tastingNote5: "Passion Fruit",
+    "returnColumn\r": "Sauvignon Blanc\r"
+  },
+  {
+    id: "61",
+    style: "Sémillon",
+    tastingNote1: "Lemon",
+    tastingNote2: "Beeswax",
+    tastingNote3: "Peach",
+    tastingNote4: "Chamomile",
+    tastingNote5: "Saline",
+    "returnColumn\r": "Sémillon\r"
+  },
+  {
+    id: "62",
+    style: "Sherry",
+    tastingNote1: "Jackfruit",
+    tastingNote2: "Saline",
+    tastingNote3: "Preserved Lemon",
+    tastingNote4: "Brazil Nut",
+    tastingNote5: "Almond",
+    "returnColumn\r": "Sherry\r"
+  },
+  {
+    id: "63",
+    style: "St. Laurent",
+    tastingNote1: "Raspberry",
+    tastingNote2: "Blackberry",
+    tastingNote3: "Sweet Tobacco",
+    tastingNote4: "Baking Spices",
+    tastingNote5: "Chocolate",
+    "returnColumn\r": "St. Laurent\r"
+  },
+  {
+    id: "64",
+    style: "Syrah / Shiraz",
+    tastingNote1: "Blueberry",
+    tastingNote2: "Black Plum",
+    tastingNote3: "Milk Chocolate",
+    tastingNote4: "Tobacco",
+    tastingNote5: "Green Peppercorn",
+    "returnColumn\r": "Syrah / Shiraz\r"
+  },
+  {
+    id: "65",
+    style: "Tempranillo",
+    tastingNote1: "Cherry",
+    tastingNote2: "Dried Fig",
+    tastingNote3: "Cedar",
+    tastingNote4: "Tobacco",
+    tastingNote5: "Dill",
+    "returnColumn\r": "Tempranillo\r"
+  },
+  {
+    id: "66",
+    style: "Torrontes",
+    tastingNote1: "Meyer Lemon",
+    tastingNote2: "Peach",
+    tastingNote3: "Rose Petal",
+    tastingNote4: "Geranium",
+    tastingNote5: "Citrus Zest",
+    "returnColumn\r": "Torrontes\r"
+  },
+  {
+    id: "67",
+    style: "Touriga Nacional",
+    tastingNote1: "Blueberry",
+    tastingNote2: "Violet",
+    tastingNote3: "Plum",
+    tastingNote4: "Mint",
+    tastingNote5: "Wet Slate",
+    "returnColumn\r": "Touriga Nacional\r"
+  },
+  {
+    id: "68",
+    style: "Trebbiano Toscano",
+    tastingNote1: "White Peach",
+    tastingNote2: "Lemon",
+    tastingNote3: "Green Apple",
+    tastingNote4: "Seashell",
+    tastingNote5: "Basil",
+    "returnColumn\r": "Trebbiano Toscano\r"
+  },
+  {
+    id: "69",
+    style: "Trento",
+    tastingNote1: "Citrus",
+    tastingNote2: "Yellow Apple",
+    tastingNote3: "White Cherry",
+    tastingNote4: "Almond",
+    tastingNote5: "Toast",
+    "returnColumn\r": "Trento\r"
+  },
+  {
+    id: "70",
+    style: "Verdejo",
+    tastingNote1: "Lime",
+    tastingNote2: "Honeydew",
+    tastingNote3: "Grapefruit Pith",
+    tastingNote4: "Fennel",
+    tastingNote5: "White Peach",
+    "returnColumn\r": "Verdejo\r"
+  },
+  {
+    id: "71",
+    style: "Vermentino",
+    tastingNote1: "Lime",
+    tastingNote2: "Grapefruit",
+    tastingNote3: "Green Apple",
+    tastingNote4: "Almond",
+    tastingNote5: "Daffodil",
+    "returnColumn\r": "Vermentino\r"
+  },
+  {
+    id: "72",
+    style: "Vin Santo",
+    tastingNote1: "Perfume",
+    tastingNote2: "Dried Fig",
+    tastingNote3: "Raisin",
+    tastingNote4: "Almond",
+    tastingNote5: "Toffee",
+    "returnColumn\r": "Vin Santo\r"
+  },
+  {
+    id: "73",
+    style: "Vinho Verde",
+    tastingNote1: "Lemonade",
+    tastingNote2: "Pink Grapefruit",
+    tastingNote3: "Lime Zest",
+    tastingNote4: "Yellow Apple",
+    tastingNote5: "White Blossom",
+    "returnColumn\r": "Vinho Verde\r"
+  },
+  {
+    id: "74",
+    style: "Viognier",
+    tastingNote1: "Tangerine",
+    tastingNote2: "Peach",
+    tastingNote3: "Mango",
+    tastingNote4: "Honeysuckle",
+    tastingNote5: "Rose",
+    "returnColumn\r": "Viognier\r"
+  },
+  {
+    id: "75",
+    style: "Viura",
+    tastingNote1: "Honeydew",
+    tastingNote2: "Lime Peel",
+    tastingNote3: "Lemon Verbana",
+    tastingNote4: "Tarragon",
+    tastingNote5: "Hazelnut",
+    "returnColumn\r": "Viura\r"
+  },
+  {
+    id: "76",
+    style: "Vouvray",
+    tastingNote1: "Lemon",
+    tastingNote2: "Apricot",
+    tastingNote3: "Pear",
+    tastingNote4: "Honey",
+    tastingNote5: "Chamomile",
+    "returnColumn\r": "Vouvray\r"
+  },
+  {
+    id: "77",
+    style: "Zinfandel",
+    tastingNote1: "Blackberry",
+    tastingNote2: "Strawberry",
+    tastingNote3: "Peach Preserves",
+    tastingNote4: "Cinnamon",
+    tastingNote5: "Sweet Tobacco",
+    "returnColumn\r": "Zinfandel\r"
+  },
+  {
+    id: "78",
+    style: "Zweigelt",
+    tastingNote1: "Red Cherry",
+    tastingNote2: "Raspberry",
+    tastingNote3: "Black Pepper",
+    tastingNote4: "Licorice",
+    tastingNote5: "Chocolate",
+    "returnColumn\r": "Zweigelt"
+  }
+];
