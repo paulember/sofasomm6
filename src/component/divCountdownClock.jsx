16cfdb2227eaa43f1342391e1338e17e4e9b6942
Paul Ember <133266739+paulember@users.noreply.github.com>
2025-05-27 09:22:50 -0400

    Create divCountdownClock.jsx
    adding new - will need pull to sofasomm6 codespaces

---
+++ src/component/divCountdownClock.jsx
@@ -0,0 +1,27 @@
+divCountdownClock.jsxfunction divCountdownClock({
+  timeLeftHH,
+  timeLeftMM,
+  timeLeftSS,
+  julianDate,
+  lastJulianPlayed,
+}) {
+  if (julianDate != lastJulianPlayed) {
+    return <div></div>;
+  } else {
+    return (
+      <table className="notesTable">
+        <tr>
+          <td>lastJulPlayed-julianDate</td>
+          <td>####{lastJulianPlayed}</td>
+          <td>####{julianDate}</td>
+        </tr>
+        <tr>
+          <td>Next Tasting</td>
+          <td>{`${timeLeftHH}:${timeLeftMM}:${timeLeftSS}`}</td>
+        </tr>
+      </table>
+    );
+  }
+}
+
+export default divCountdownClock;
